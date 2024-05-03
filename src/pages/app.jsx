af5ae2ba29a18c0ee5f453cc4ed3d19c6ab0f969
Audiomuse1 <andrewhartford@utexas.edu>
2024-05-02 21:05:31 -0500

    Edit title

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -8,7 +8,7 @@ export default function AppPage() {
   return (
     <>
       <Helmet>
-        <title> Dashboard | Minimal UI </title>
+        <title> Andrew Hartford - Mancomm Dashboard Exercise</title>
       </Helmet>
 
       <AppView />
