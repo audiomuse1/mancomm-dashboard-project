054ca25eda47fe2beeb7119c505b2fbac880fb7e
Audiomuse1 <andrewhartford@utexas.edu>
2024-05-05 12:08:39 -0500

    Logo added

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -3,20 +3,20 @@ import { forwardRef } from 'react';
 
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import { useTheme } from '@mui/material/styles';
+// import { useTheme } from '@mui/material/styles';
 
 import { RouterLink } from 'src/routes/components';
 
 // ----------------------------------------------------------------------
 
 const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
-  const theme = useTheme();
+  // const theme = useTheme();
 
-  const PRIMARY_LIGHT = theme.palette.primary.light;
+  // const PRIMARY_LIGHT = theme.palette.primary.light;
 
-  const PRIMARY_MAIN = theme.palette.primary.main;
+  // const PRIMARY_MAIN = theme.palette.primary.main;
 
-  const PRIMARY_DARK = theme.palette.primary.dark;
+  // const PRIMARY_DARK = theme.palette.primary.dark;
 
   // OR using local (public folder)
   // -------------------------------------------------------
@@ -33,14 +33,19 @@ const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
       ref={ref}
       component="div"
       sx={{
-        width: 40,
-        height: 40,
+        width: 150,
+        height: 'auto',
         display: 'inline-flex',
         ...sx,
       }}
       {...other}
     >
-      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 512 512">
+
+      {/* Using Mancomm logo */}
+      <img src="/assets/logo.png" alt="Logo" style={{ width: '100%', height: '100%' }} />
+
+      
+      {/* <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 512 512">
         <defs>
           <linearGradient id="BG1" x1="100%" x2="50%" y1="9.946%" y2="50%">
             <stop offset="0%" stopColor={PRIMARY_DARK} />
@@ -48,6 +53,7 @@ const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
           </linearGradient>
 
           <linearGradient id="BG2" x1="50%" x2="50%" y1="0%" y2="100%">
+            <stop offset="0%" stopColor={PRIMARY_LIGHT} />
             <stop offset="0%" stopColor={PRIMARY_LIGHT} />
             <stop offset="100%" stopColor={PRIMARY_MAIN} />
           </linearGradient>
@@ -72,7 +78,7 @@ const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
             d="M450 384c26.509 0 48-21.491 48-48s-21.491-48-48-48-48 21.491-48 48 21.491 48 48 48"
           />
         </g>
-      </svg>
+      </svg> */}
     </Box>
   );
 
